99c3dad03dd905fa35ce295ca96a29c7267c002f
Evan BOISSONNOT <evan.boissonnot@netil.fr>
2017-05-07 23:41:22 +0200

    travail sur la création de la base de données

---
+++ CetaitQuoiLeContexte/Boissonnot.Framework.Core.Interfaces/Data/IDatabaseInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boissonnot.Framework.Core.Interfaces.Data
+{
+    public interface IDatabaseInitializer
+    {
+        /// <summary>
+        /// Initialisation de la base de données
+        /// </summary>
+        /// <param name="context">Context à initialiser</param>
+        void Initialize(DbContext context);
+    }
+}
---
+++ CetaitQuoiLeContexte/CetaitQuoiLeContexte.Core.Data/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using CetaitQuoiLeContexte.Core.Interfaces.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CetaitQuoiLeContexte.Core.Data
+{
+    public class DbInitializer : Boissonnot.Framework.Core.Interfaces.Data.IDatabaseInitializer
+    {
+        #region Public methods
+        public void Initialize(DbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+        #endregion
+    }
+}
