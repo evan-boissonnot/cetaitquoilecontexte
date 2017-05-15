bd0c9217319b67c1a3f3278c7d375b22244c76bf
Evan BOISSONNOT <evan.boissonnot@netil.fr>
2017-05-16 00:37:32 +0200

    Préparation pour enregistrement

---
+++ CetaitQuoiLeContexte/CetaitQuoiLeContext.Core.Web/Controllers/ContextController.cs
@@ -0,0 +1,42 @@
+﻿using CetaitQuoiLeContexte.Core.Interfaces.Business;
+using CetaitQuoiLeContexte.Core.Interfaces.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CetaitQuoiLeContext.Core.Web.Controllers
+{
+    public class ContextController : Controller
+    {
+        #region Fields
+        private IContextBusiness _business = null;
+        private IServiceProvider _serviceProvider = null;
+        #endregion
+
+        #region Constructors
+        public ContextController(IContextBusiness business, IServiceProvider serviceProvider)
+        {
+            this._business = business;
+            this._serviceProvider = serviceProvider;
+        }
+        #endregion
+
+        #region Public methods
+
+        public IActionResult Add(string value)
+        {
+            IContext context = null;
+
+            dynamic result = new {
+                State = "OK",
+                Item = context
+            };
+
+
+
+            return this.Json(result);
+        }
+        #endregion  
+    }
+}
--- CetaitQuoiLeContexte/CetaitQuoiLeContexte.Web.UI/wwwroot/js/site.js
+++ CetaitQuoiLeContexte/CetaitQuoiLeContexte.Web.UI/wwwroot/js/site.js
@@ -2,6 +2,15 @@
 (function ($) {
     "use strict"; 
 
+    $("#btnAddContext").click(function () {
+        $.ajax({
+            url: '/Context/Add',
+            dataType: "json",
+            data: { value: $("#txtContent").val() }
+        }).success(function () {
+            $("#txtContent").empty();
 
 
+        });
+    });
 })(jQuery); 
