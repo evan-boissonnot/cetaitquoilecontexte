0367064b36c0f66e8f990fe072db2f53aec2a741
Evan BOISSONNOT <evan.boissonnot@netil.fr>
2017-05-16 16:14:31 +0200

    Fin affichage nouveau contexte RAF : gestion du retour à la ligne en html

--- CetaitQuoiLeContexte/CetaitQuoiLeContexte.Web.UI/wwwroot/js/site.js
+++ CetaitQuoiLeContexte/CetaitQuoiLeContexte.Web.UI/wwwroot/js/site.js
@@ -2,15 +2,33 @@
 (function ($) {
     "use strict"; 
 
+    var shortHtmlTemplate = $("#template-short-context").html();
+    var longHtmlTemplate = $("#template-long-context").html();
+    var $shortContextList = $("#short-list");
+    var $longContextList = $("#long-list");
+
     $("#btnAddContext").click(function () {
         $.ajax({
             url: '/Context/Add',
             dataType: "json",
             data: { value: $("#txtContent").val() }
-        }).success(function () {
-            $("#txtContent").empty();
+        }).success(function (data) {
+            $("#txtContent").val('');
 
+            if (data.state == "OK") {
+                replaceHtmlContent(data, shortHtmlTemplate, $shortContextList);
+                replaceHtmlContent(data, longHtmlTemplate, $longContextList);
+            }
 
         });
     });
+
+    function replaceHtmlContent(data, htmlContent, $list) {
+        var newShortContext = htmlContent;
+
+        newShortContext = newShortContext.replace(/\{0\}/gi, data.item.id);
+        newShortContext = newShortContext.replace(/\{1\}/gi, data.item.message);
+
+        $list.append(newShortContext);
+    };
 })(jQuery); 
